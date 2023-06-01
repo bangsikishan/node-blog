7269ac37c09f39425ebda141a7d76e2b0f84147e
Kishan <bangsikishan@duck.com>
2023-06-01 10:30:05 +0545

    Added search functionality

--- server/routes/main.js
+++ server/routes/main.js
@@ -24,8 +24,35 @@ router.get("/", async (req, res) => {
 });
 
 
-router.get("/about", (req, res) => {
-    res.render('index');
+router.get('/post/:id', async (req, res) => {
+    try {
+        let slug = req.params.id;
+
+        const data = await Post.findById({ _id: slug });
+        res.render('post', { data });
+    } catch (error) {
+        console.log(error);
+    }
 });
+
+
+router.post('/search', async (req, res) => {
+    try {
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+        
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i') } }
+            ]
+        });
+
+        res.render('search', { data });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 module.exports = router;
