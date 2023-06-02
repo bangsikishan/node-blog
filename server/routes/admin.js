9d4e1361025ee91ca839019c9561f48816a08cd5
Kishan <bangsikishan@duck.com>
2023-06-02 10:25:15 +0545

    Added delete route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -115,4 +115,50 @@ router.post('/add-post', authMiddleware, async (req, res) => {
     }
 });
 
+
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+        const data = await Post.findOne({ _id: req.params.id });
+
+        res.render('/admin/admin-post', { data, layout: adminLayout });
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+        await Post.findByIdAndUpdate(req.params.id, { 
+            title: req.body.title,
+            body: req.body.body,
+            updatedAt: Date.now()
+        });
+
+        res.redirect(`/edit-post/${req.params.id}`);
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
+    try {
+        await Post.deleteOne({ _id: req.params.id });
+
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/');
+});
+
+
 module.exports = router;
